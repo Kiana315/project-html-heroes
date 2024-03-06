78fa9f92f4347ffa4b27e33275fc730a31a6a0b5
Kiana Yu <qianya1@ualberta.ca>
2024-03-05 21:34:42 -0700

    Fix edit post
    User could edit visibility and content type

--- SocialDistribution/static/js/editing.js
+++ SocialDistribution/static/js/editing.js
@@ -9,6 +9,7 @@ document.addEventListener('DOMContentLoaded', function() {
         editModal.style.display = 'block';  // Show up edit modal
         options.style.display = 'none';     // Hide menu
 
+        // fetch origin post data
         fetch(`/api/posts/${postId}/`)
             .then(response => {
                 if (!response.ok) {
@@ -20,6 +21,15 @@ document.addEventListener('DOMContentLoaded', function() {
                 // Load the obtained data into the form
                 document.getElementById('titleInput').value = data.title;
                 document.getElementById('contentInput').value = data.content;
+                document.getElementById('content_type').checked = data.content_type === 'COMMONMARK'; 
+                const visibilitySelect = document.getElementById('visibility');
+                for (let i = 0; i < visibilitySelect.options.length; i++) {
+                    if (visibilitySelect.options[i].value === data.visibility) {
+                        visibilitySelect.selectedIndex = i;
+                        break;
+                    }
+                }
+
             })
             .catch(error => {
                 console.error('Error fetching post data:', error);
@@ -41,14 +51,22 @@ document.addEventListener('DOMContentLoaded', function() {
         // Get new data from form
         const updatedTitle = document.getElementById('titleInput').value;
         const updatedContent = document.getElementById('contentInput').value;
+        const updatedContentType = document.getElementById('content_type').checked ? 'COMMONMARK' : 'PLAIN'; 
+        const updatedVisibility = document.getElementById('visibility').value;
         
+
         fetch(`/api/posts/${postId}/update/`, {
             method: 'PUT',  
             headers: {
                 'Content-Type': 'application/json',
                 'X-CSRFToken': getCookie('csrftoken')  
             },
-            body: JSON.stringify({ title: updatedTitle, content: updatedContent })
+            body: JSON.stringify({ 
+                title: updatedTitle, 
+                content: updatedContent,
+                content_type: updatedContentType,
+                visibility: updatedVisibility
+             })
         })
         .then(response => {
             if (!response.ok) {
