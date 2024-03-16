4bc1c1d8b41d1e5cfc87b266a90f3d09b7556d36
elialex15 <ealexand@ualberta.ca>
2024-03-16 14:00:13 -0600

    sign up settings created on runserver

--- SocialDistribution/apps.py
+++ SocialDistribution/apps.py
@@ -5,3 +5,7 @@ class SocialdistributionConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "SocialDistribution"
 
+    def ready(self):
+        from .models import SignUpSettings
+        if not SignUpSettings.objects.exists():
+            SignUpSettings.objects.create()
